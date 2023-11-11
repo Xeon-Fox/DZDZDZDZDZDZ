e7501577bec326625d00258900eefcf282829784
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2023-11-11 20:16:53 +0600

    zabil zapustit programmu

--- 11.11.23/runnercopy.ps1
+++ 11.11.23/runnercopy.ps1
@@ -1,7 +1,7 @@
 cd C:\Users\Xeon_Fox\Documents\GitHub\DZDZDZDZDZDZ\11.11.23previousdz
 ls
 start previousdz.txt
-cd ..
+cd C:\Users\Xeon_Fox\Documents\GitHub\DZDZDZDZDZDZ\11.11.23
 echo "" > runnercopy.ps1
 cp runner.ps1 runnercopy.ps1
 ls
