fe3e156c4195d92ce5407f5a246de16b0c70f7c4
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-11-12 23:21:02 +0600

    Проверено дз от 11.11
    Комментарии к коду

--- 11.11.23/runner.ps1
+++ 11.11.23/runner.ps1
@@ -1,7 +1,12 @@
 cd C:\Users\Xeon_Fox\Documents\GitHub\DZDZDZDZDZDZ\11.11.23previousdz
+#VN: ^^^ здесь лучше указывать относительный путь, а не абсолютный
+
 ls
 start previousdz.txt
+#VN: в том каталоге должны быть .py файлы, и запускать их нужно по-другому
+
 cd C:\Users\Xeon_Fox\Documents\GitHub\DZDZDZDZDZDZ\11.11.23
 echo "" > runnercopy.ps1
+#VN: ^^^ эта строка лишняя, так как команда cp сама создаст новый файл 
 cp runner.ps1 runnercopy.ps1
 ls
