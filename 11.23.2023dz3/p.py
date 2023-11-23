8cec65abc95c7606dce52f2cbf1fbe8bcbf57318
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2023-11-23 19:15:41 +0600

    дз3

---
+++ 11.23.2023dz3/p.py
@@ -0,0 +1,21 @@
+#3
+firstnumberepicbattlecalculator=float(input("еен "))
+secondnumberepicbattlecalculator=float(input("леен2 "))
+pluspppp=firstnumberepicbattlecalculator+secondnumberepicbattlecalculator
+minsuyas=firstnumberepicbattlecalculator-secondnumberepicbattlecalculator
+umnozit=firstnumberepicbattlecalculator*secondnumberepicbattlecalculator
+delit=firstnumberepicbattlecalculator/secondnumberepicbattlecalculator
+print(pluspppp," ",minsuyas," ",umnozit," ", delit)
+
+#2
+KilometersVvednniChelovekom=float(input("не позволите ли вы вписать километы "))
+NatoAmericanMileIhateamerica=float
+NatoAmericanMileIhateamerica=0.62
+kmmileburgeramericailoveamerica=KilometersVvednniChelovekom*NatoAmericanMileIhateamerica
+print(kmmileburgeramericailoveamerica)
+
+#1
+p = float(input("перимтр квадрата: "))
+a = p / 4
+s = a * a
+ print("Площадь квадрта равна", s)
