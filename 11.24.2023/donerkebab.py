2925c75705b41d8981dc24bc0ff482bdb4468b56
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2023-11-24 19:17:30 +0600

    Create donerkebab.py

---
+++ 11.24.2023/donerkebab.py
@@ -0,0 +1,45 @@
+#1 chislo_zachemto_isportit_kotoroe=int(input("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.: "))
+# binarchisloperemenaoguzok=bin(chislo_zachemto_isportit_kotoroe)
+# vosemchislodropdesateptvoiu=oct(chislo_zachemto_isportit_kotoroe)
+# desat=hex(chislo_zachemto_isportit_kotoroe)
+# print('1 ', binarchisloperemenaoguzok, '2 ', vosemchislodropdesateptvoiu, '3 ', desat)
+
+#2 uskoreneefizikagdz7klass=float(input("a: "))
+# velocityoffiziksinthetask=float(input("v= "))
+# time_vremia_t_shitjopa=float(input("t являеться чем "))
+# s=(uskoreneefizikagdz7klass*time_vremia_t_shitjopa**2)/2 + velocityoffiziksinthetask * time_vremia_t_shitjopa
+# print("s=", s)
+
+#3 x1_coordinate_algebra_10klass_gdz=float(input("x1 оордината "))
+# y1_absis_piska_jopa=float(input("y1 координата абцис "))
+# x2_coordinate_algebra_10klass_gdz=float(input("x2 оордината "))
+# y2_absis_piska_jopa=float(input("y2 координата абцис "))
+# rasstoyanie=math.sqrt((x2_coordinate_algebra_10klass_gdz-x1_coordinate_algebra_10klass_gdz)**2+(y2_absis_piska_jopa-y1_absis_piska_jopa)**2)
+# print(rasstoyanie)
+
+#4 firstchetotameptvoyou=float(input("asdasd "))
+# secondshitpoststatus=float(input("asdaszxc "))
+# x=-(secondshitpoststatus/firstchetotameptvoyou)
+# print(x)
+
+#5 hours_current_time_world_usa_kosovo=int(input("chas= "))
+# minutes_current_time_world_usa_kosovo=int(input("minutes= "))
+# dayintheworldhowmuch=23
+# minuteshowmuchisit= 60
+# dayhours=dayintheworldhowmuch-hours_current_time_world_usa_kosovo
+# minutesshit=minuteshowmuchisit-minutes_current_time_world_usa_kosovo // 1
+# print(dayhours," часов ", minutesshit, " минут")
+
+#6 number_mumber_humber_dumber = int(input("Введите трехзначное число: "))
+# number_mumber_humber_dumber = number_mumber_humber_dumber // 10 % 10
+# second_digit_cifra_number = number_mumber_humber_dumber
+# print("Вторая цифра числа:", second_digit_cifra_number)
+
+#7 не понел как
+
+#8
+social_viploti_ot_gosudarstva=250
+prodanno_shit_a_month=int(input("драг диллер тэвион работает на банду бладс и продает крэк, на какую сумму он продал крэк сегодня?: "))
+prodanno_shit_a_month = prodanno_shit_a_month * 0.1
+overall_zarplata_bro = prodanno_shit_a_month+social_viploti_ot_gosudarstva
+print("тэвион заработал ", overall_zarplata_bro, " долларов и он скорее всего потратит их на крэк и сторчиться к 21 году")
