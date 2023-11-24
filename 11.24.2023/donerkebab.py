dcea69b8c267833a83b8f240bb0dabc440c2db04
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2023-11-24 19:42:54 +0600

    добавил деталей

--- 11.24.2023/donerkebab.py
+++ 11.24.2023/donerkebab.py
@@ -42,4 +42,4 @@ social_viploti_ot_gosudarstva=250
 prodanno_shit_a_month=int(input("драг диллер тэвион работает на банду бладс и продает крэк, на какую сумму он продал крэк сегодня?: "))
 prodanno_shit_a_month = prodanno_shit_a_month * 0.1
 overall_zarplata_bro = prodanno_shit_a_month+social_viploti_ot_gosudarstva
-print("тэвион заработал ", overall_zarplata_bro, " долларов и он скорее всего потратит их на крэк и сторчиться к 21 году")
+print("тэвион заработал ", overall_zarplata_bro, " долларов, и разделил их среди своих близких хоуми с района. Однако он скорее всего потратит свою долю на крэк, и сторчиться к 21 году")
