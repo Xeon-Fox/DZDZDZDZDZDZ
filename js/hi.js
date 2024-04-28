67b7ccaa815b81ba3cfe9ee91f8c0dcada8fa689
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2024-04-28 17:52:06 +0500

    d

--- js/hi.js
+++ js/hi.js
@@ -1,195 +1,29 @@
-// 1
-let age = prompt("ваш возраст");
-if (age >= 0 && age <= 2){
-    alert("ребенок");
-}
-else if (age >= 12 && age <= 18){
-    alert("подросток");
-}
-else if (age >= 18 && age <= 60){
-    alert("взрослый");
-}
-else if (age > 60) {
-    alert("старина");
-}
-
-// 2
-let num = prompt("число от 0 до 9");
-switch (num) {
-    case '1':
-        alert("!");
-        break;
-    case '2':
-        alert("@");
-        break;
-    case '3':
-        alert("#");
-        break;
-    // дальше мне лень ну там понятно да
-}
-
-// 3
-let num = prompt("трехзначное число");
-let digits = num.split("");
-if(digits[0] == digits[1] || digits[0] == digits[2] || digits[1] == digits[2]){
-    alert("одинаковые цифры");
-}
-else {
-    alert("нет одинаковых цифр");
-}
-
-// 4
-let year = prompt("введите год");
-if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
-    alert(" високосный");
-}
-else {
-    alert("эне високосный");
-}
-
-//5
-let num = prompt(" число");
-let digits = num.split("");
-if(digits[0] == digits[4] && digits[1] == digits[3]){
-    alert("палиндромом");
-}
-else {
-    alert("не палиндромом");
-}
-
-let amount = prompt("деньги");
-let currency = prompt("выберите валюту");
-let result;
-switch (currency) {
-    case 'EUR':
-        result = amount * 1; 
-        break;
-    case 'UAN':
-        result = amount * 2;
-        break;
-    case 'AZN':
-        result = amount * 3; 
-        break;
-}
-alert(`сумма в ${currency}: ${result}`);
-
-//7
-let sum = prompt("Введите сумму покупки");
-let discount;
-if (sum >= 200 && sum < 300) {
-    discount = 0.03;
-} else if (sum >= 300 && sum < 500) {
-    discount = 0.05;
-} else if (sum >= 500) {
-    discount = 0.07;
-}
-let total = sum - sum * discount;
-alert(`Сумма к оплате сс скидкой: ${total}`);
-
-// 8
-let circleLength = prompt("Введите длину окружности");
-let squarePerimeter = prompt("Введите периметр квадрата");
-let circleDiameter = circleLength / Math.PI;
-let squareSide = squarePerimeter / 4;
-if (circleDiameter <= squareSide) {
-    alert("Окружность может поместиться в квадрат")
-}
-else {
-    alert("Окружность не может поместиться в квадрат")
-}
-
-//9
-let score = 0;
-let question1 = prompt("вопрос!@!!");
-if (question1 == '2') {
-    score += 2;
-}
-// тоже самое 2 раза
-alert(`Вы набрали ${score} баллов`);
-
-//10
-let date = new Date(prompt("Введите дату (день, месяц, год)"));
-date.setDate(date.getDate() + 1);
-alert(`Следующая дата: ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`);
-
-/1
-function compareNumbers(a, b) {
-    if (a < b) {
-        return -1;
-    } else if (a > b) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
+//1
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(Math.max(...numbers), Math.min(...numbers))
 
 //2
-
-function factorial(n) {
-    let result = 1;
-    for (let i = 2; i <= n; i++) {
-        result *= i;
-    }
-    return result;
+let numbersPrompt = []
+let sum = 0
+for(let i = 0; i < 5; i++) {
+    numbersPrompt[i] = Number(prompt("число"))
+    sum += numbersPrompt[i]
 }
+console.log(`сумма: ${sum}, сред: ${sum / numbersPrompt.length}`)
 
-
-//3
-function digitsToNumber(a, b, c) {
-    return a * 100 + b * 10 + c;
+// 3
+let array1 = [1, 2, 3, 4, 5, 6, 7]
+let array2 = [7, 6, 5, 4, 3, 2, 1]
+for(let i = 0; i < 7; i++) {
+    console.log(`${i}: в 1 - ${array1[i]}, во 2 - ${array2[i]}`)
 }
 
 //4
-function rectangleArea(length, width = length) {
-    return length * width;
-}
-//5
-function isPerfectNumber(n) {
-    let sum = 0;
-    for (let i = 1; i <= n / 2; i++) {
-        if (n % i === 0) {
-            sum += i;
-        }
+let numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let evenNumbers = []
+for(let i = 0; i < numbers1.length; i++) {
+    if(numbers1[i] % 2 === 0) {
+        evenNumbers.push(numbers1[i])
     }
-    return sum === n;
 }
-
-//6
-function perfectNumbersInRange(min, max) {
-    let result = [];
-    for (let i = min; i <= max; i++) {
-        if (isPerfectNumber(i)) {
-            result.push(i);
-        }
-    }
-    return result;
-}
-
-//7и8
-function formatTime(hours, minutes = "00", seconds = "00") {
-    return `${hours}:${minutes}:${seconds}`;
-}
-
-//9
-function timeToSeconds(hours, minutes, seconds) {
-    return hours * 3600 + minutes * 60 + seconds;
-}
-//10
-function secondsToTime(seconds) {
-    let hours = Math.floor(seconds / 3600);
-    seconds %= 3600;
-    let minutes = Math.floor(seconds / 60);
-    seconds %= 60;
-    return formatTime(hours, minutes, seconds);
-}
-
-//11
-function timeDifference(year1, month1, day1, year2, month2, day2) {
-    let date1 = new Date(year1, month1 - 1, day1);
-    let date2 = new Date(year2, month2 - 1, day2);
-    let differenceInSeconds = Math.abs(date1 - date2) / 1000;
-    return secondsToTime(differenceInSeconds);
-}
-
-
+console.log(evenNumbers)
--- sql/1.sql
+++ sql/1.sql
@@ -1,91 +1,180 @@
-CREATE DATABASE Airport;
+create table author (
+	id int identity primary key,
+  	fio nvarchar(100) not null,
+  	bdate datetime
+)
 
-USE Airport;
+create table book (
+	id int identity primary key,
+  	bookName nvarchar(100) not null,
+  	authorId int foreign key references author(id),
+  	publicationYear datetime ,
+  	isReturn bit default 0
+)
 
-CREATE TABLE Airlines (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(255),
-    country VARCHAR(255),
-    PRIMARY KEY(id)
+create table reader (
+	id int identity PRIMARY key,
+  	fio nvarchar(100) not null,
+  	bdate datetime not null,
+  	phone nvarchar(20) not null,
+  	address nvarchar(100) not null
+)
+
+create table orders (
+	id int identity primary key,
+  	bookId int foreign key references book(id),
+  	readerId int foreign key references reader(id),
+  	startDate datetime ,
+  	endDate datetime,
+  	isBlackList bit default 0
+)
+
+
+update book
+set isreturn = 1
+where id % 3 = 2
+
+
+update orders
+set isBlackList = 1
+where id % 10 = 0
+
+
+update orders
+set endDate = dateadd(day, 30, startdate)
+
+
+
+CREATE table students (
+ id int identity primary key,
+   fullname nvarchar(100) not null,
+   bdate datetime not null
+)
+CREATE table groups (
+ id int identity primary key,
+   groupname nvarchar(100) not null
+)
+create table groupsstudents (
+ id int identity primary key,
+   groupsid int foreign key references groups(id),
+   studentsid int foreign key references students(id)
+)
+         
+create table Student (
+  	id int identity primary key,
+	FirstName VARCHAR(50),
+	LastName VARCHAR(50),
+	Email VARCHAR(50),
+	BirthDate DATE
 );
+  
 
-CREATE TABLE Airplanes (
-    id INT AUTO_INCREMENT,
-    model VARCHAR(255),
-    airline_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY (airline_id) REFERENCES Airlines(id)
+
+create table Achievements (
+  	id int identity primary key,
+  	Assesment int not null,
+	StudentId INT FOREIGN key references Student(id)
 );
+               
+create table section 
+(
+	id int identity primary key,
+	name nvarchar(50) not null unique
+)
 
-CREATE TABLE Airports (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(255),
-    city VARCHAR(255),
-    country VARCHAR(255),
-    PRIMARY KEY(id)
-);
+create table class 
+(
+	id int identity primary key,
+	name nvarchar(50) not null unique
+)
 
-CREATE TABLE Flights (
-    id INT AUTO_INCREMENT,
-    departure_airport_id INT,
-    arrival_airport_id INT,
-    departure_time DATETIME,
-    arrival_time DATETIME,
-    airplane_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY (departure_airport_id) REFERENCES Airports(id),
-    FOREIGN KEY (arrival_airport_id) REFERENCES Airports(id),
-    FOREIGN KEY (airplane_id) REFERENCES Airplanes(id)
-);
+create table instructor
+(
+	id int identity primary key,
+	fio nvarchar(100) not null,
+	nick nvarchar(15) not null,
+	workExperience int not null,
+	price int not null,
+	sectionId int foreign key references section(id),
+	classId int foreign key references class(id)
+)
 
-CREATE TABLE Passengers (
-    id INT AUTO_INCREMENT,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    passport_number VARCHAR(255),
-    PRIMARY KEY(id)
-);
+create table client 
+(
+	id int identity primary key,
+	name nvarchar(50) not null unique,
+	instructorId int foreign key references instructor(id)
+)
 
-CREATE TABLE Tickets (
-    id INT AUTO_INCREMENT,
-    flight_id INT,
-    passenger_id INT,
-    seat_number VARCHAR(255),
-    class ENUM('Economy', 'Business', 'First'),
-    price DECIMAL(10, 2),
-    PRIMARY KEY(id),
-    FOREIGN KEY (flight_id) REFERENCES Flights(id),
-    FOREIGN KEY (passenger_id) REFERENCES Passengers(id)
-);
+-- 1
+SELECT GroupName, DepartmentName 
+FROM Groups 
+JOIN Departments ON Groups.DepartmentID = Departments.ID 
+WHERE Year > DATEADD(year, -5, GETDATE());
 
-CREATE TABLE Employees (
-    id INT AUTO_INCREMENT,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    position VARCHAR(255),
-    airport_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY (airport_id) REFERENCES Airports(id)
-);
+--2
+SELECT TOP 1 Name 
+FROM Teachers 
+ORDER BY Salary DESC;
+
+--3
+SELECT TOP 1 FacultyName 
+FROM Faculties 
+ORDER BY Funding ASC;
 
 
+--4
+SELECT COUNT(*) 
+FROM Groups 
+WHERE Schedule LIKE '%Monday%';
 
 
+--5
+SELECT LectureRoom, TeacherName, Subject 
+FROM Lectures 
+JOIN Teachers ON Lectures.TeacherID = Teachers.ID 
+WHERE Teachers.Salary > 10000;
 
-INSERT INTO Airlines (name, country) VALUES ('флайарыстан', 'кз'), ('флайдубай', 'оаэ');
-INSERT INTO Airplanes (model, airline_id) VALUES ('боинг 747', 1), ('боинг 777', 2);
-INSERT INTO Airports (name, city, country) VALUES ('аэропорт алмата', 'алмата', 'кз'), ('аэропорт дубая', 'дубай', 'оаэ');
-INSERT INTO Flights (departure_airport_id, arrival_airport_id, departure_time, arrival_time, airplane_id) VALUES (1, 2, '2024-01-01 10:00:00', '2024-01-01 14:00:00', 1);
-INSERT INTO Passengers (first_name, last_name, passport_number) VALUES ('сигма', 'крипер', '123456789'), ('кай', 'сенат', '987654321');
-INSERT INTO Tickets (flight_id, passenger_id, seat_number, class, price) VALUES (1, 1, '12A', 'экокном', 100.00), (1, 2, '12B', 'бизнес', 100.00);
-INSERT INTO Employees (first_name, last_name, position, airport_id) VALUES ('красава', 'крутой', 'капитан', 1), ('девушка', 'женщина', 'бортпроводница', 2);
+-- 6
+SELECT Year, COUNT(*) 
+FROM Departments 
+JOIN Faculties ON Departments.FacultyID = Faculties.ID 
+WHERE Faculties.Name LIKE 'A%' 
+GROUP BY Year;
+
+-- 7
+SELECT FacultyName, SUM(Funding) 
+FROM Faculties 
+GROUP BY FacultyName;
+
+--8
+SELECT TeacherName, GroupName 
+FROM Teachers 
+JOIN Groups ON Teachers.GroupID = Groups.ID 
+WHERE GroupName LIKE '%[0-9]%';
 
 
+--9
+SELECT COUNT(*), TeacherName 
+FROM Subjects 
+JOIN Teachers ON Subjects.TeacherID = Teachers.ID 
+GROUP BY TeacherName;
+
+--10
+SELECT FacultyName, DepartmentName 
+FROM Faculties 
+JOIN Departments ON Faculties.ID = Departments.FacultyID 
+ORDER BY Faculties.Funding ASC, Departments.Funding ASC
+
+---11
+SELECT GroupName, LectureRoom 
+FROM Lectures 
+JOIN Groups ON Lectures.GroupID = Groups.ID 
+WHERE DayOfWeek = 'Monday';
 
 
-SELECT Flights.id, departure_time, arrival_time, Airlines.name AS airline, Airplanes.model AS airplane, departure.name AS departure_airport, arrival.name AS arrival_airport
-FROM Flights
-JOIN Airplanes ON Flights.airplane_id = Airplanes.id
-JOIN Airlines ON Airplanes.airline_id = Airlines.id
-JOIN Airports AS departure ON Flights.departure_airport_id = departure.id
-JOIN Airports AS arrival ON Flights.arrival_airport_id = arrival.id;
+--12
+SELECT Salary, TeacherName 
+FROM Teachers 
+JOIN Subjects ON Teachers.ID = Subjects.TeacherID 
+WHERE Subjects.Name LIKE '%22%';
